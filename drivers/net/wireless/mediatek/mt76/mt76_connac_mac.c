ede11461682c3ed6062ff748624bda4c44da315a
Ben Greear <greearb@candelatech.com>
2022-01-17 14:15:56 -0800

    mt76: mt7915/7921: add rx rate idx histogram to ethtool stats.
    To provide user information on RX packet encoding stats.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt76_connac_mac.c
+++ drivers/net/wireless/mediatek/mt76/mt76_connac_mac.c
@@ -1089,6 +1089,12 @@ int mt76_connac2_mac_fill_rx_rate(struct mt76_dev *dev,
 			*nss = 2;
 		else
 			*nss = 1;
+		if (stats) {
+			if (unlikely(i > 11))
+				stats->rx_rate_idx[11]++;
+			else
+				stats->rx_rate_idx[i]++;
+		}
 		break;
 	case MT_PHY_TYPE_HT_GF:
 	case MT_PHY_TYPE_HT:
@@ -1100,6 +1106,10 @@ int mt76_connac2_mac_fill_rx_rate(struct mt76_dev *dev,
 			mib->rx_d_bad_ht_rix++;
 			return -EINVAL;
 		}
+		if (stats) {
+			int rix = i % 8;
+			stats->rx_rate_idx[rix]++;
+		}
 		break;
 	case MT_PHY_TYPE_VHT:
 		status->encoding = RX_ENC_VHT;
@@ -1109,6 +1119,8 @@ int mt76_connac2_mac_fill_rx_rate(struct mt76_dev *dev,
 			mib->rx_d_bad_vht_rix++;
 			return -EINVAL;
 		}
+		if (stats)
+			stats->rx_rate_idx[i]++;
 		break;
 	case MT_PHY_TYPE_HE_MU:
 	case MT_PHY_TYPE_HE_SU:
@@ -1121,6 +1133,12 @@ int mt76_connac2_mac_fill_rx_rate(struct mt76_dev *dev,
 			status->he_gi = gi;
 
 		status->he_dcm = dcm;
+		if (stats) {
+			if (unlikely(i > 11))
+				stats->rx_rate_idx[11]++;
+			else
+				stats->rx_rate_idx[i]++;
+		}
 		break;
 	default:
 		mib->rx_d_bad_mode++;
