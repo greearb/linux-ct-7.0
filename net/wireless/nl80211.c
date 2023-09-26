fe252086d572613d6626add01c7efe85b1c7e350
Ben Greear <greearb@candelatech.com>
2023-09-26 14:00:52 -0700

    wifi: Add scan failure logging.
    To try to understand why mt7921 stas sometimes report EINVAL
    repeatedly.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -10799,6 +10799,8 @@ static int nl80211_trigger_scan(struct sk_buff *skb, struct genl_info *info)
 			chan = ieee80211_get_channel_khz(wiphy, freq);
 			if (!chan) {
 				err = -EINVAL;
+				pr_err("scan: get-channel failed, freq: %d\n",
+				       freq);
 				goto out_free;
 			}
 
@@ -10863,6 +10865,8 @@ static int nl80211_trigger_scan(struct sk_buff *skb, struct genl_info *info)
 		nla_for_each_nested(attr, info->attrs[NL80211_ATTR_SCAN_SSIDS], tmp) {
 			if (nla_len(attr) > IEEE80211_MAX_SSID_LEN) {
 				err = -EINVAL;
+				pr_err("scan: ssid is out of range, len: %d\n",
+				       nla_len(attr));
 				goto out_free;
 			}
 			request->req.ssids[i].ssid_len = nla_len(attr);
@@ -10892,6 +10896,8 @@ static int nl80211_trigger_scan(struct sk_buff *skb, struct genl_info *info)
 
 			if (band < 0 || band >= NUM_NL80211_BANDS) {
 				err = -EINVAL;
+				pr_err("scan: band is out of range: %d\n",
+				       band);
 				goto out_free;
 			}
 
@@ -10902,8 +10908,11 @@ static int nl80211_trigger_scan(struct sk_buff *skb, struct genl_info *info)
 						     nla_data(attr),
 						     nla_len(attr),
 						     &request->req.rates[band]);
-			if (err)
+			if (err) {
+				pr_err("scan: get-ratemask failed: %d\n",
+				       err);
 				goto out_free;
+			}
 		}
 	}
 
@@ -10915,8 +10924,11 @@ static int nl80211_trigger_scan(struct sk_buff *skb, struct genl_info *info)
 	}
 
 	err = nl80211_check_scan_flags_reg(wiphy, wdev, info->attrs, request);
-	if (err)
+	if (err) {
+		pr_err("scan: check-scan-flags failed: %d\n",
+		       err);
 		goto out_free;
+	}
 
 	request->req.no_cck =
 		nla_get_flag(info->attrs[NL80211_ATTR_TX_NO_CCK_RATE]);
@@ -10950,8 +10962,11 @@ static int nl80211_trigger_scan(struct sk_buff *skb, struct genl_info *info)
 	rdev->scan_req = request;
 	err = cfg80211_scan(rdev);
 
-	if (err)
+	if (err) {
+		pr_err("scan: cfg80211_scan failed: %d\n",
+		       err);
 		goto out_free;
+	}
 
 	nl80211_send_scan_start(rdev, wdev);
 	dev_hold(wdev->netdev);
