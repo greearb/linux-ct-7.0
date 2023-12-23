994f8f6e7a70d9e0a189228889871c6225bfd941
Eric Woudstra <ericwouds@gmail.com>
2023-12-23 20:09:25 +0100

    net: sfp: Fixup for OEM SFP-2.5G-T(-R-RM) module
    Change from quirk to fixup for the OEM SFP-2.5G-T module.
    Add OEM SFP-2.5G-T-R-RM module.
    
    Implementing this fixup, the rtl8221b phy is attached and the quirk is
    no longer used.
    
    The module is re-branded to different brands:
    
    OEM SFP-2.5G-T: visible brand LuLeey.
    Add SFP-2.5G-T-R-RM: no visible brand, bought from Sinovoip store
    
    Signed-off-by: Eric Woudstra <ericwouds@gmail.com>

--- drivers/net/phy/sfp.c
+++ drivers/net/phy/sfp.c
@@ -414,6 +414,13 @@ static void sfp_fixup_rollball_wait4s(struct sfp *sfp)
 	sfp->module_t_wait = msecs_to_jiffies(4000);
 }
 
+// For 2.5GBASE-T short-reach modules
+static void sfp_fixup_oem_2_5gbaset(struct sfp *sfp)
+{
+	sfp_fixup_rollball(sfp);
+	sfp->id.base.extended_cc = SFF8024_ECC_2_5GBASE_T;
+}
+
 static void sfp_fixup_fs_10gt(struct sfp *sfp)
 {
 	sfp_fixup_10gbaset_30m(sfp);
@@ -459,6 +466,7 @@ static void sfp_quirk_2500basex(const struct sfp_eeprom_id *id,
 	__set_bit(PHY_INTERFACE_MODE_2500BASEX, caps->interfaces);
 }
 
+#if 0
 static void sfp_quirk_disable_autoneg(const struct sfp_eeprom_id *id,
 				      struct sfp_module_caps *caps)
 {
@@ -474,6 +482,7 @@ static void sfp_quirk_oem_2_5g(const struct sfp_eeprom_id *id,
 	__set_bit(PHY_INTERFACE_MODE_2500BASEX, caps->interfaces);
 	sfp_quirk_disable_autoneg(id, caps);
 }
+#endif
 
 static void sfp_quirk_ubnt_uf_instant(const struct sfp_eeprom_id *id,
 				      struct sfp_module_caps *caps)
@@ -580,7 +589,8 @@ static const struct sfp_quirk sfp_quirks[] = {
 	SFP_QUIRK_F("OEM", "SFP-GE-T", sfp_fixup_ignore_tx_fault),
 
 	SFP_QUIRK_F("OEM", "SFP-10G-T", sfp_fixup_rollball_cc),
-	SFP_QUIRK_S("OEM", "SFP-2.5G-T", sfp_quirk_oem_2_5g),
+	SFP_QUIRK_F("OEM", "SFP-2.5G-T", sfp_fixup_oem_2_5gbaset),
+	SFP_QUIRK_F("OEM", "SFP-2.5G-T-R-RM", sfp_fixup_oem_2_5gbaset),
 	SFP_QUIRK_S("OEM", "SFP-2.5G-BX10-D", sfp_quirk_2500basex),
 	SFP_QUIRK_S("OEM", "SFP-2.5G-BX10-U", sfp_quirk_2500basex),
 	SFP_QUIRK_F("OEM", "RTSFP-10", sfp_fixup_rollball_cc),
