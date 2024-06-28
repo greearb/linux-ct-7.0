779be6bc495a816df7bd3f5dce8bd667b20d3147
Ben Greear <greearb@candelatech.com>
2024-06-28 08:29:02 -0700

    mt7996: Enable puncturing support.
    Ported mostly from mtk owrt feed patch, looks like I had some older
    version of this code in there already, so that is updated.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7996/mt7996.h
+++ drivers/net/wireless/mediatek/mt76/mt7996/mt7996.h
@@ -379,8 +379,10 @@ struct mt7996_phy {
 	bool counter_reset;
 	bool rdd_tx_paused;
 
-	bool sr_enable:1;
-	bool enhanced_sr_enable:1;
+	bool sr_enable;
+	bool enhanced_sr_enable;
+	u8 pp_mode;
+	u16 punct_bitmap;
 };
 
 struct mt7996_dev {
@@ -876,8 +878,7 @@ int mt7996_mcu_wtbl_update_hdr_trans(struct mt7996_dev *dev,
 				     struct mt7996_vif_link *link,
 				     struct mt7996_sta_link *msta_link);
 int mt7996_mcu_cp_support(struct mt7996_dev *dev, u8 mode);
-int mt7996_mcu_set_pp_en(struct mt7996_phy *phy, bool auto_mode, u8 force_bitmap,
-			 u16 bitmap);
+int mt7996_mcu_set_pp_en(struct mt7996_phy *phy, u8 mode, u16 bitmap);
 #ifdef CONFIG_MAC80211_DEBUGFS
 void mt7996_sta_add_debugfs(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 			    struct ieee80211_sta *sta, struct dentry *dir);
