c2954e172dc2f526c7c4f04546de5954ead3d60a
Dylan Eskew <dylan.eskew@candelatech.com>
2024-09-24 12:13:10 -0700

    mac80211: add ability to ignore EDCA in stations
    For the purposes of testing - apparently common - broken
    stations.
    
    Signed-off-by: Dylan Eskew <dylan.eskew@candelatech.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -12741,6 +12741,8 @@ static int nl80211_associate(struct sk_buff *skb, struct genl_info *info)
 				req.flags |= ASSOC_REQ_DISABLE_320;
 			if (cai->flags & CT_DISABLE_OFDMA)
 				req.flags |= ASSOC_REQ_DISABLE_OFDMA;
+			if (cai->flags & CT_IGNORE_EDCA)
+				req.flags |= ASSOC_REQ_IGNORE_EDCA;
 		}
 	}
 skip_ct_priv:
@@ -13640,6 +13642,8 @@ static int nl80211_connect(struct sk_buff *skb, struct genl_info *info)
 				connect.flags |= ASSOC_REQ_DISABLE_320;
 			if (cai->flags & CT_DISABLE_OFDMA)
 				connect.flags |= ASSOC_REQ_DISABLE_OFDMA;
+			if (cai->flags & CT_IGNORE_EDCA)
+				connect.flags |= ASSOC_REQ_IGNORE_EDCA;
 		}
 	}
 	skip_ct_priv:
