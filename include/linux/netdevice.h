greearb/linux-ct-3.14

netdev: Notify-queue-woken callback hooks.
Allow callers to register to be notified when netdev is available
for transmit again.

Signed-off-by: Ben Greear <greearb@candelatech.com>

include/linux/netdevice.h
@@ -198,6 +198,7 @@ extern struct static_key rps_needed;
 struct neighbour;
 struct neigh_parms;
 struct sk_buff;
+struct pktgen_dev;
 
 struct netdev_hw_addr {
 	struct list_head	list;
@@ -1438,6 +1439,14 @@ struct net_device {
 	/* MRP */
 	struct mrp_port __rcu	*mrp_port;
 
+	 /* Callback for when the queue is woken, used by pktgen currently */
+	int                     (*notify_queue_woken)(struct net_device *dev);
+	void* nqw_data; /* To be used by the method above as needed */
+
+	struct pktgen_dev* pkt_dev; /* to quickly find the pkt-gen dev registered with this
+				     * interface, if any.
+				     */
+
 	/* class/net/name entry */
 	struct device		dev;
 	/* space for optional device, statistics, and wireless sysfs groups */
@@ -2052,8 +2061,11 @@ void __netif_schedule(struct Qdisc *q);
 
 static inline void netif_schedule_queue(struct netdev_queue *txq)
 {
-	if (!(txq->state & QUEUE_STATE_ANY_XOFF))
+	if (!(txq->state & QUEUE_STATE_ANY_XOFF)) {
 		__netif_schedule(txq->qdisc);
+		if (txq->dev->notify_queue_woken)
+			txq->dev->notify_queue_woken(txq->dev);
+	}
 }
 
 static inline void netif_tx_schedule_all(struct net_device *dev)
@@ -2098,8 +2110,11 @@ static inline void netif_tx_wake_queue(struct netdev_queue *dev_queue)
 		return;
 	}
 #endif
-	if (test_and_clear_bit(__QUEUE_STATE_DRV_XOFF, &dev_queue->state))
+	if (test_and_clear_bit(__QUEUE_STATE_DRV_XOFF, &dev_queue->state)) {
 		__netif_schedule(dev_queue->qdisc);
+		if (dev_queue->dev->notify_queue_woken)
+			dev_queue->dev->notify_queue_woken(dev_queue->dev);
+	}
 }
 
 /**
@@ -2383,8 +2398,11 @@ static inline void netif_wake_subqueue(struct net_device *dev, u16 queue_index)
 	if (netpoll_trap())
 		return;
 #endif
-	if (test_and_clear_bit(__QUEUE_STATE_DRV_XOFF, &txq->state))
+	if (test_and_clear_bit(__QUEUE_STATE_DRV_XOFF, &txq->state)) {
 		__netif_schedule(txq->qdisc);
+		if (dev->notify_queue_woken)
+			dev->notify_queue_woken(dev);
+	}
 }
 
 #ifdef CONFIG_XPS

greearb/linux-ct-7.0

include/linux/netdevice.h
@@ -233,6 +233,7 @@ struct net_device_core_stats {
 struct neighbour;
 struct neigh_parms;
 struct sk_buff;
+struct pktgen_dev;
 
 struct netdev_hw_addr {
 	struct list_head	list;
@@ -2416,6 +2417,14 @@ struct net_device {
 #if IS_ENABLED(CONFIG_NET_DROP_MONITOR)
 	struct dm_hw_stat_delta __rcu *dm_private;
 #endif
+
+	 /* Callback for when the queue is woken, used by pktgen currently */
+	int                     (*notify_queue_woken)(struct net_device *dev);
+	void* nqw_data; /* To be used by the method above as needed */
+
+	struct pktgen_dev* pkt_dev; /* to quickly find the pkt-gen dev registered with this
+				     * interface, if any.
+				     */
 	struct device		dev;
 	const struct attribute_group *sysfs_groups[5];
 	const struct attribute_group *sysfs_rx_queue_group;
@@ -3631,6 +3640,9 @@ static inline void netif_tx_schedule_all(struct net_device *dev)
 
 	for (i = 0; i < dev->num_tx_queues; i++)
 		netif_schedule_queue(netdev_get_tx_queue(dev, i));
+
+	if (dev->notify_queue_woken)
+		dev->notify_queue_woken(dev);
 }
 
 static __always_inline void netif_tx_start_queue(struct netdev_queue *dev_queue)
@@ -3909,8 +3921,11 @@ static inline void netdev_tx_completed_queue(struct netdev_queue *dev_queue,
 	if (unlikely(dql_avail(&dev_queue->dql) < 0))
 		return;
 
-	if (test_and_clear_bit(__QUEUE_STATE_STACK_XOFF, &dev_queue->state))
+	if (test_and_clear_bit(__QUEUE_STATE_STACK_XOFF, &dev_queue->state)) {
 		netif_schedule_queue(dev_queue);
+		if (dev_queue->dev->notify_queue_woken)
+			dev_queue->dev->notify_queue_woken(dev_queue->dev);
+	}
 #endif
 }
 
@@ -4877,6 +4892,8 @@ static inline void netif_tx_disable(struct net_device *dev)
 	}
 	spin_unlock(&dev->tx_global_lock);
 	local_bh_enable();
+	if (dev->notify_queue_woken)
+		dev->notify_queue_woken(dev);
 }
 
 #ifndef CONFIG_PREEMPT_RT