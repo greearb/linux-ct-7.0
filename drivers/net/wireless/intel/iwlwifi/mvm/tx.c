greearb/linux-ct-6.8

wifi: iwlwifi: Enable setting beamforming for TXO.
And fix parsing first token in the txo setting logic in debugfs.

Signed-off-by: Ben Greear <greearb@candelatech.com>

drivers/net/wireless/intel/iwlwifi/mvm/tx.c
@@ -370,8 +370,8 @@ static u32 iwl_mvm_get_txo_rate_n_flags(struct iwl_mvm *mvm, struct iwl_txo_data
 		goto check_v1;
 	} else if (td->tx_rate_mode == 4) { /* HE-SU */
 		/* V2 format */
-		u8 mcs = td->tx_rate_idx;
-		u8 nss = td->tx_rate_nss;
+		u32 mcs = td->tx_rate_idx;
+		u32 nss = td->tx_rate_nss;
 
 		result = RATE_MCS_HE_MSK; /* HE-SU by default, others possible */
 		result |= mcs;
@@ -391,12 +391,15 @@ static u32 iwl_mvm_get_txo_rate_n_flags(struct iwl_mvm *mvm, struct iwl_txo_data
 			result |= LQ_SS_STBC_1SS_ALLOWED;
 
 		result |= ((u32)(td->tx_rate_sgi)) << RATE_MCS_HE_GI_LTF_POS;
+		if (td->beamforming)
+			result |= (1<<19); /* beamforming on */
+		/* pr_info("TXO he-su, v2 rate-n-flags: 0x%x\n", result); */
 	} else if (td->tx_rate_mode == 5) { /* EHT */
 		/* V2 format */
-		u8 mcs = td->tx_rate_idx;
-		u8 nss = td->tx_rate_nss;
+		u32 mcs = td->tx_rate_idx;
+		u32 nss = td->tx_rate_nss;
 
-		result = RATE_MCS_EHT_MSK; /* HE-SU by default, others possible */
+		result = RATE_MCS_EHT_MSK; /* EHT-SU by default, others possible */
 		result |= mcs;
 		result |= nss << RATE_MCS_NSS_POS;
 
@@ -414,6 +417,8 @@ static u32 iwl_mvm_get_txo_rate_n_flags(struct iwl_mvm *mvm, struct iwl_txo_data
 			result |= RATE_MCS_LDPC_MSK;
 		if (td->stbc)
 			result |= LQ_SS_STBC_1SS_ALLOWED;
+		if (td->beamforming)
+			result |= (1<<19); /* beamforming on */
 
 		result |= ((u32)(td->tx_rate_sgi)) << RATE_MCS_HE_GI_LTF_POS;
 	}

greearb/linux-ct-7.0

drivers/net/wireless/intel/iwlwifi/mvm/tx.c
@@ -367,8 +367,8 @@ static u32 iwl_mvm_get_txo_rate_n_flags(struct iwl_mvm *mvm, struct iwl_txo_data
 		goto check_v1;
 	} else if (td->tx_rate_mode == 4) { /* HE-SU */
 		/* V2 format */
-		u8 mcs = td->tx_rate_idx;
-		u8 nss = td->tx_rate_nss;
+		u32 mcs = td->tx_rate_idx;
+		u32 nss = td->tx_rate_nss;
 
 		result = RATE_MCS_MOD_TYPE_HE; /* HE-SU by default, others possible */
 		result |= mcs;
@@ -388,12 +388,15 @@ static u32 iwl_mvm_get_txo_rate_n_flags(struct iwl_mvm *mvm, struct iwl_txo_data
 			result |= LQ_SS_STBC_1SS_ALLOWED;
 
 		result |= ((u32)(td->tx_rate_sgi)) << RATE_MCS_HE_GI_LTF_POS;
+		if (td->beamforming)
+			result |= (1<<19); /* beamforming on */
+		/* pr_info("TXO he-su, v2 rate-n-flags: 0x%x\n", result); */
 	} else if (td->tx_rate_mode == 5) { /* EHT */
 		/* V2 format */
-		u8 mcs = td->tx_rate_idx;
-		u8 nss = td->tx_rate_nss;
+		u32 mcs = td->tx_rate_idx;
+		u32 nss = td->tx_rate_nss;
 
-		result = RATE_MCS_MOD_TYPE_EHT; /* HE-SU by default, others possible */
+		result = RATE_MCS_MOD_TYPE_EHT; /* EHT-SU by default, others possible */
 		result |= mcs;
 		result |= u32_encode_bits(nss, RATE_MCS_NSS_MSK);
 
@@ -411,6 +414,8 @@ static u32 iwl_mvm_get_txo_rate_n_flags(struct iwl_mvm *mvm, struct iwl_txo_data
 			result |= RATE_MCS_LDPC_MSK;
 		if (td->stbc)
 			result |= LQ_SS_STBC_1SS_ALLOWED;
+		if (td->beamforming)
+			result |= (1<<19); /* beamforming on */
 
 		result |= ((u32)(td->tx_rate_sgi)) << RATE_MCS_HE_GI_LTF_POS;
 	}