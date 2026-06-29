greearb/linux-ct-6.17

wifi: mt76: Block tx if waiting too long to hear from any MSDUs.
For reasons unknown, certain MSDUs seem to not be returned via a TXFREE
until every other MSDU has been handled. If there is a consistent Tx
stream from another source, this can cause those MPDUs to stay blocked
indefinitely. It should be noted that we are not observing any reported
retries or Tx failures for these blocked MPDUs, they just seem to not be
reported. The packets appear to be transmitted soon after DMA, MSDUs
are just not reported back with a TXFREE.

This is resolved by tagging the enqueue time of each MSDU, and keeping
a priority queue of all currently DMA'd MSDUs. All of the operations
that we need (check the timestamp of the oldest MSDU, enqueue, remove)
can be done in constant time due to the idr already tracking the MSDU's
tokens, and so there is very little overhead for this check.

If we see a particular MSDU not freed quickly enough, block all Tx until
it is freed. We have never observed an instance where these MSDUs have
not eventually been freed in this case.

Signed-off-by: Rory Little <rory@candelatech.com>

drivers/net/wireless/mediatek/mt76/tx.c
@@ -5,6 +5,11 @@
 
 #include "mt76.h"
 
+static unsigned long tx_wait_thresh_ms = 100;
+module_param_named(tx_wait_thresh_ms, tx_wait_thresh_ms, ulong, 0644);
+MODULE_PARM_DESC(tx_wait_thresh_ms, "Time to wait for TXFREE before flushing the tx queue.\n"
+				    "0 to disable this behavior.");
+
 static int
 mt76_txq_get_qid(struct ieee80211_txq *txq)
 {
@@ -956,8 +961,14 @@ int mt76_token_consume(struct mt76_dev *dev, struct mt76_txwi_cache **ptxwi)
 	spin_lock_bh(&dev->token_lock);
 
 	token = idr_alloc(&dev->token, *ptxwi, 0, dev->token_size, GFP_ATOMIC);
-	if (token >= 0)
+	if (token >= 0) {
 		dev->token_count++;
+
+		list_add(&(*ptxwi)->list, dev->token_queue_tail);
+		dev->token_queue_tail = &(*ptxwi)->list;
+
+		(*ptxwi)->jiffies = jiffies;
+	}
 
 #ifdef CONFIG_NET_MEDIATEK_SOC_WED
 	if (mtk_wed_device_active(&dev->mmio.wed) &&
@@ -995,13 +1006,17 @@ EXPORT_SYMBOL_GPL(mt76_rx_token_consume);
 struct mt76_txwi_cache *
 mt76_token_release(struct mt76_dev *dev, int token, bool *wake)
 {
-	struct mt76_txwi_cache *txwi;
+	struct mt76_txwi_cache *txwi, *oldest_txwi;
 
 	spin_lock_bh(&dev->token_lock);
 
 	txwi = idr_remove(&dev->token, token);
 	if (txwi) {
 		dev->token_count--;
+
+		if (dev->token_queue_tail == &txwi->list)
+			dev->token_queue_tail = txwi->list.prev;
+		list_del(&txwi->list);
 
 #ifdef CONFIG_NET_MEDIATEK_SOC_WED
 		if (mtk_wed_device_active(&dev->mmio.wed) &&
@@ -1011,9 +1026,19 @@ mt76_token_release(struct mt76_dev *dev, int token, bool *wake)
 #endif
 	}
 
-	if (dev->token_count < dev->token_size - MT76_TOKEN_FREE_THR &&
-	    dev->phy.q_tx[0]->blocked)
+	oldest_txwi = list_first_entry_or_null(&dev->token_queue, struct mt76_txwi_cache, list);
+
+	if (tx_wait_thresh_ms && oldest_txwi &&
+	    time_is_before_jiffies(oldest_txwi->jiffies + (HZ * tx_wait_thresh_ms / 1000))) {
+		if (!dev->phy.q_tx[0]->blocked)
+			mt76_dbg(dev, MT76_DBG_WRN,
+				 "Tx queue blocked, clearing before allowing more transmits.");
+		__mt76_set_tx_blocked(dev, true);
+	} else if (dev->token_count < dev->token_size - MT76_TOKEN_FREE_THR &&
+		   dev->phy.q_tx[0]->blocked && !*wake) {
+		mt76_dbg(dev, MT76_DBG_WRN, "Restarting previously blocked queue.");
 		*wake = true;
+	}
 
 	spin_unlock_bh(&dev->token_lock);
 

greearb/linux-ct-7.0

drivers/net/wireless/mediatek/mt76/tx.c
@@ -5,6 +5,11 @@
 
 #include "mt76.h"
 
+static unsigned long tx_wait_thresh_ms = 100;
+module_param_named(tx_wait_thresh_ms, tx_wait_thresh_ms, ulong, 0644);
+MODULE_PARM_DESC(tx_wait_thresh_ms, "Time to wait for TXFREE before flushing the tx queue.\n"
+				    "0 to disable this behavior.");
+
 static int
 mt76_txq_get_qid(struct ieee80211_txq *txq)
 {
@@ -958,8 +963,14 @@ int mt76_token_consume(struct mt76_dev *dev, struct mt76_txwi_cache **ptxwi)
 	token = idr_alloc(&dev->token, *ptxwi, dev->token_start,
 			  dev->token_start + dev->token_size,
 			  GFP_ATOMIC);
-	if (token >= dev->token_start)
+	if (token >= dev->token_start) {
 		dev->token_count++;
+
+		list_add(&(*ptxwi)->list, dev->token_queue_tail);
+		dev->token_queue_tail = &(*ptxwi)->list;
+
+		(*ptxwi)->jiffies = jiffies;
+	}
 
 #ifdef CONFIG_NET_MEDIATEK_SOC_WED
 	if (mtk_wed_device_active(&dev->mmio.wed) &&
@@ -997,13 +1008,17 @@ EXPORT_SYMBOL_GPL(mt76_rx_token_consume);
 struct mt76_txwi_cache *
 mt76_token_release(struct mt76_dev *dev, int token, bool *wake)
 {
-	struct mt76_txwi_cache *txwi;
+	struct mt76_txwi_cache *txwi, *oldest_txwi;
 
 	spin_lock_bh(&dev->token_lock);
 
 	txwi = idr_remove(&dev->token, token);
 	if (txwi) {
 		dev->token_count--;
+
+		if (dev->token_queue_tail == &txwi->list)
+			dev->token_queue_tail = txwi->list.prev;
+		list_del(&txwi->list);
 
 #ifdef CONFIG_NET_MEDIATEK_SOC_WED
 		if (mtk_wed_device_active(&dev->mmio.wed) &&
@@ -1013,9 +1028,19 @@ mt76_token_release(struct mt76_dev *dev, int token, bool *wake)
 #endif
 	}
 
-	if (dev->token_count < dev->token_size - MT76_TOKEN_FREE_THR &&
-	    dev->phy.q_tx[0]->blocked)
+	oldest_txwi = list_first_entry_or_null(&dev->token_queue, struct mt76_txwi_cache, list);
+
+	if (tx_wait_thresh_ms && oldest_txwi &&
+	    time_is_before_jiffies(oldest_txwi->jiffies + (HZ * tx_wait_thresh_ms / 1000))) {
+		if (!dev->phy.q_tx[0]->blocked)
+			mt76_dbg(dev, MT76_DBG_TXV,
+				 "Tx queue blocked, clearing before allowing more transmits.");
+		__mt76_set_tx_blocked(dev, true);
+	} else if (dev->token_count < dev->token_size - MT76_TOKEN_FREE_THR &&
+		   dev->phy.q_tx[0]->blocked && !*wake) {
+		mt76_dbg(dev, MT76_DBG_TXV, "Restarting previously blocked queue.");
 		*wake = true;
+	}
 
 	spin_unlock_bh(&dev->token_lock);
 